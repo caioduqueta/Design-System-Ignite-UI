40785cd1f59e98604bfefe7a5558326331d441b9
caioduqueta <caio.duqueta@gmail.com>
2023-01-31 13:08:41 -0300

    ci:Add Turborepo cache on deploy build

--- packages/react/src/styles/index.ts
+++ packages/react/src/styles/index.ts
@@ -6,7 +6,7 @@ import {
   lineHeights,
   radii,
   space,
-} from '@ignite-ui/tokens'
+} from '@duqueta-ignite-ui/tokens'
 import { createStitches, defaultThemeMap } from '@stitches/react'
 
 export const {

7030604a69e9cfe5a9a0f2cd1248be93b8c915e7
caioduqueta <caio.duqueta@gmail.com>
2023-01-31 10:19:52 -0300

    Add core Components

---
+++ packages/docs/src/stories/Checkbox.stories.tsx
@@ -0,0 +1,38 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Box, Text, Checkbox, CheckboxProps } from '@ignite-ui/react'
+export default {
+  title: 'Form/Checkbox',
+  component: Checkbox,
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'row', gap: '$2' }}
+        >
+          {Story()}
+          <Text size="sm">Accept Terms of use</Text>
+        </Box>
+      )
+    },
+  ],
+} as Meta<CheckboxProps>
+
+export const Primary: StoryObj<CheckboxProps> = {
+  args: {
+    placeholder: 'type your name',
+  },
+}
+
+export const Disabled: StoryObj<CheckboxProps> = {
+  args: {
+    disabled: true,
+  },
+}
+
+export const WithPrefix: StoryObj<CheckboxProps> = {
+  args: {
+    prefix: 'cal.com/',
+  },
+}
